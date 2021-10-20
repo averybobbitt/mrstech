02a5d8fd63bfe906e027ae5a16ecd9de8c0dc4c5
avery <avery@bobbitt.dev>
2021-10-20 00:22:24 -0400

    converted all modules to es6 and all for loops to lambdas (very broken)

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,15 +11,18 @@ const commands = [];
 const commandFiles = readdirSync("./commands").filter((file) => file.endsWith(".js"));
 
 // Push each command to commands array
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+commandFiles.forEach((file) => {
+  const command = await import(`./commands/${file}`);
+
+  console.log(`loaded: ${command}`);
+  console.log(`\t${command.data}`);
   commands.push(command.data.toJSON());
-}
+});
 
 // Push commands to server
 const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
 rest
-  .put(Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD), { body: commands })
+  .put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
   .then(() => console.log("Successfully registered application commands."))
   .catch(console.error);
--- index.js
+++ index.js
@@ -11,24 +11,24 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Initialize commands
 client.commands = new Collection();
-const commandDir = readdirSync("./commands").filter((file) => file.endsWith(".js"));
+const commandFiles = readdirSync("./commands").filter((file) => file.endsWith(".js"));
 
-for (const file of commandDir) {
-  const command = require(`./commands/${file}`);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
-  client.commands.set(command.data.name, command);
-}
+// Set a new item in the Collection with the key as the command name and the value as the exported module
+commandFiles.forEach((file) =>
+  import(`./commands/${file}`).then((command) => client.commands.set(command.data.name, command))
+);
 
 // Event handling
 const events = readdirSync("./events").filter((file) => file.endsWith(".js"));
-for (const file of events) {
-  const event = require(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
-}
+events.forEach((file) =>
+  import(`./events/${file}`).then((event) => {
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+  })
+);
 
 // Login to Discord with the client's token
 client.login(process.env.TOKEN);
