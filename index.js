0abac55bb3e4a0c602bcb4d0e8c0686b03a4a2f0
Avery Bobbitt <avery@bobbitt.dev>
2023-01-24 14:39:53 -0500

    added correct imports for REST and Routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary classes
 const fs = require("fs");
 const dotenv = require("dotenv");
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -19,30 +19,31 @@ client.login(process.env.TOKEN);
 
 // initialize and handle commands
 function init_commands() {
-    const commands = [];
-    const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+  const commands = [];
+  const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
 
-    // Push each command to commands array
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        commands.push(command.data.toJSON());
-    }
+  // Push each command to commands array
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+  }
 
-    // Push commands to server
-    rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
-        .then(() => console.log("Successfully registered application commands."))
-        .catch(console.error);
+  // Push commands to server
+  rest
+    .put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
+    .then(() => console.log("Successfully registered application commands."))
+    .catch(console.error);
 }
 
 // initialize and handle events
 function init_events() {
-    const events = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
-    for (const file of events) {
-        const event = require(`./events/${file}`);
-        if (event.once) {
-            client.once(event.name, (...args) => event.execute(...args));
-        } else {
-            client.on(event.name, (...args) => event.execute(...args));
-        }
+  const events = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
+  for (const file of events) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
     }
+  }
 }
